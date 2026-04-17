45c44a5e099547d592ea96ae221718220393f87c
Amigo <90645586+AlenGacic@users.noreply.github.com>
2026-04-17 22:48:48 +0200

    quest 8 completed

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...theArgs){
+    console.log(arr, theArgs)
+    let newArr = arr.filter((x)=> !theArgs.includes(x))
+    console.log(newArr)
+    return newArr
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
