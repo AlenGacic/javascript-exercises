a595e46060e9551b6f6385548e930e7cd252083b
Amigo <90645586+AlenGacic@users.noreply.github.com>
2026-04-17 23:52:18 +0200

    quest 9 completed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 ){
+        return 'ERROR';
+    }else if(!Number.isInteger(x) || !Number.isInteger(y)){
+        return 'ERROR';
+    }
+    const arr = [x,y];
+    arr.sort((a, b) => a - b);
+
+    let firstNum = arr[0]
+    let lastNum = arr[1]
+    for (let i = (firstNum + 1); i < lastNum ; i++){
+        arr.push(i);
+        // console.log(arr);
+    }
+    const sumOfall = arr.reduce((total, current) => total + current)
+    return sumOfall;
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+    
+
+//create array
+//sort it from lowest to 
