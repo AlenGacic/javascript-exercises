28345961c7cf66dfec36b07beb9d7571ca2b5712
Amigo <90645586+AlenGacic@users.noreply.github.com>
2026-04-18 15:44:50 +0200

    quest 9 completed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheirt) {
+  let celsius = (fahrenheirt - 32) * (5/9);
+  let roundCelsius = Math.round(celsius * 10) / 10
+  return roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5 + 32);
+  let roundFahrenheit = Math.round(fahrenheit * 10) / 10
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
