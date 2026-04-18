9bf9c672d8a67413225a7f667c6789275d3dbd6e
Amigo <90645586+AlenGacic@users.noreply.github.com>
2026-04-18 15:31:27 +0200

    quest 9 done

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function(x) {
+    let year = Number(x);
+        if(year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)){
+            return false;
+        }else{
+            return true;
+        }
 
-};
+}
 
 // Do not edit below this line
 module.exports = leapYears;
